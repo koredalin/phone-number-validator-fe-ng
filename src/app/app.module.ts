d26b98bac03be193d2367347c7d1de65a8a7fe71
Hristo Hristov <koredalin@gmail.com>
2021-07-28 17:13:46 +0300

    Global styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
